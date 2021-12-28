1442c42b39c88a87afd667d57342ba34feba8ae3
soumyabandihal <soumyabandihal1525@gmail.com>
2021-12-28 21:48:48 +0530

    Months

---
+++ Months.java
@@ -0,0 +1,9 @@
+class Months{
+		public static void main(String[] args){
+		
+		int m = 06;
+		if (m == 06 ){
+		System.out.println("Month :"+ "Jan" + "Feb" + "Mar" + "Arp" + "May" + "Jun" + "Jul" + "Aug" + "Sep" + "Oct" + "Nov" + "Dec");
+		}
+	}
+}
