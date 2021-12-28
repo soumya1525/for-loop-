cee6bb5224547236596925b12aecf66360c39497
soumyabandihal <soumyabandihal1525@gmail.com>
2021-12-28 22:51:07 +0530

    Group members Numbers

---
+++ MobileNo.java
@@ -0,0 +1,12 @@
+class MobileNo{
+
+	public static void main(String[] args){
+
+	String []phoneNo = {"9482736775","9731008824","8123019051","8147214046","8296796488","7892721595","9535658430"};
+
+				
+		System.out.println(phoneNo[0]+ "," +phoneNo[1]+ "," +phoneNo[2]+ "," + phoneNo[3]+ "," +phoneNo[4]+ "," + phoneNo[5]+ "," + phoneNo[6]);
+		
+	}
+	
+}
