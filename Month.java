6742b37f52c2437d612bcecf5896637dc9d68155
soumyabandihal <soumyabandihal1525@gmail.com>
2021-12-28 21:46:34 +0530

    Month

---
+++ Month.java
@@ -0,0 +1,45 @@
+class Months{
+		public static void main(String[] args){
+		
+		int m = 1;
+		if(m==1){
+		System.out.println("JAN");
+		
+		}else if (m == 2){
+		System.out.println("FEB");
+			
+		}else if (m == 3){
+		System.out.println("MAR");
+				
+		}else if (m == 4){
+		System.out.println("ARP");
+	
+		}else if (m == 5){
+		System.out.println("MAY");
+						
+		}else if (m == 6){
+		System.out.println("JUN");
+		
+		}else if (m == 7){
+		System.out.println("JUL");
+			
+		}else if (m == 8){
+		System.out.println("AUG");
+				
+		}else if (m == 9){
+		System.out.println("SEP");
+	
+		}else if (m == 10){
+		System.out.println("OCT");
+						
+		}else if (m == 11){
+		System.out.println("NOV");
+		
+		}else if(m == 12){
+		System.out.println("Dec");
+
+		
+		}
+
+	}
+}
