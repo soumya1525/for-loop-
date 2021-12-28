fe225b7250372d181ebabffc333d3b569f8efcd9
soumyabandihal <soumyabandihal1525@gmail.com>
2021-12-28 21:35:02 +0530

    Weekday

---
+++ Weekday.java
@@ -0,0 +1,23 @@
+class Weekday{
+		public static void main(String[] args){
+			int days = 1;
+			if(days == 1 ){
+			System.out.println("SUNDAY");
+			}
+			else if (days == 2){
+				System.out.println("MONDAY");
+			}else if (days == 3){
+				System.out.println("TUESDAY");
+			}else if (days == 4){
+				System.out.println("WEDNESDAY");
+			}else if (days == 5){
+				System.out.println("THURSDAY");
+			}else if (days == 6){
+				System.out.println("FRIDAY");
+			}else if (days == 7){
+				System.out.println("SATURDAY");
+
+			}
+		}
+
+}
